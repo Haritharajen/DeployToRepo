6d73deeb2028cbd3ca06372b919c564123ebb955
at <haritharaj181167@gmail.com>
2018-05-17 15:31:29 +0530

    First commit

---
+++ TestToDeploy/src/com/asahi/testtodeploy/TestClass.java
@@ -0,0 +1,11 @@
+package com.asahi.testtodeploy;
+
+public class TestClass {
+	
+	void test() {
+		
+		System.out.println("Hai.....");
+	}
+	
+	
+}
---
+++ TestToDeploy/src/com/asahi/testtodeploy/TestClass1.java
@@ -0,0 +1,8 @@
+package com.asahi.testtodeploy;
+
+public class TestClass1 {
+	
+	void test1() {
+		System.out.println("Hello..");
+	}
+}
